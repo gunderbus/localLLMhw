b9f5b5a43d5fb720a9949f11c428aa70937156de
gunderbus <cole.gundermann@gmail.com>
2026-03-01 21:02:31 -0500

    Initial JavaFX commit

---
+++ src/main/java/cole/oen/cpoels/coekl/App.java
@@ -0,0 +1,23 @@
+package cole.oen.cpoels.coekl;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) {
+        StackPane root = new StackPane(new Label("Hello JavaFX"));
+        Scene scene = new Scene(root, 640, 400);
+
+        stage.setTitle("tuoeis");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
