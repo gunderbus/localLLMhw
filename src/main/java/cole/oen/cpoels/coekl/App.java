c988f59d9abe114896d393edd5d91218c3cab1c0
gunderbus <cole.gundermann@gmail.com>
2026-03-01 21:09:01 -0500

    edited sum stuff

--- src/main/java/cole/oen/cpoels/coekl/App.java
+++ src/main/java/cole/oen/cpoels/coekl/App.java
@@ -1,7 +1,10 @@
 package cole.oen.cpoels.coekl;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -9,8 +12,21 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage stage) {
-        StackPane root = new StackPane(new Label("Hello JavaFX"));
+        Label title = new Label("Hello JavaFX");
+        title.getStyleClass().add("title-label");
+
+        Button actionButton = new Button("Click Me");
+        actionButton.getStyleClass().add("bottom-button");
+
+        StackPane root = new StackPane(title, actionButton);
+        root.getStyleClass().add("app-root");
+        StackPane.setAlignment(actionButton, Pos.BOTTOM_CENTER);
+        StackPane.setMargin(actionButton, new Insets(0, 0, 24, 0));
+
         Scene scene = new Scene(root, 640, 400);
+        scene.getStylesheets().add(
+            App.class.getResource("/styles.css").toExternalForm()
+        );
 
         stage.setTitle("tuoeis");
         stage.setScene(scene);
